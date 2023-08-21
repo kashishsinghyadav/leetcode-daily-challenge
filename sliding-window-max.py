3cf7ddfb1b9c9e9029fb67ef82ee26d37e1fe814
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2023-08-21 21:11:18 +0530

    Create sliding-window-max.py

---
+++ sliding-window-max.py
@@ -0,0 +1,20 @@
+rom heapq import heappop, heappush, heapify
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        if not nums:
+            return []
+        if k == 1:
+            return nums
+        deq = deque()
+        result = []
+
+        for i in range(len(nums)):
+            while deq and deq[0] < i - k + 1:
+                deq.popleft()
+            while deq and nums[i] > nums[deq[-1]]:
+                deq.pop()
+            deq.append(i)
+            if i >= k - 1:
+                result.append(nums[deq[0]])      
+  
+        return result
